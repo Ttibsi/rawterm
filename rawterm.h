7795d5fc6db1b81b920ef8b0244dbcc8d446a061
Ttibsi <ashisbitt@icloud.com>
2023-09-11 18:24:47 +0100

    letters

--- rawterm.h
+++ rawterm.h
@@ -140,13 +140,139 @@ Key process_keypress() {
     }
 
 
+    // TODO: numbers, fnums, enter, escape, arrows, alt?
     // https://www.rapidtables.com/code/text/ascii-table.html
-    // Escape
-    std::cout << "sub0" << substrings[0] << "\r\n";
-    if (substrings[0] == "\x27") {
-    } else if (substrings[0] == "\\x61") {
-        std::cout << "aaaaaaa\r\n";
-    }
+    if (substrings[0] == "\\x65") {
+        k.code = 'A';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x66") {
+        k.code = 'B';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x67") {
+        k.code = 'C';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x68") {
+        k.code = 'D';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x69") {
+        k.code = 'E';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x70") {
+        k.code = 'F';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x71") {
+        k.code = 'G';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x72") {
+        k.code = 'H';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x73") {
+        k.code = 'I';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x74") {
+        k.code = 'J';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x75") {
+        k.code = 'K';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x76") {
+        k.code = 'L';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x77") {
+        k.code = 'M';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x78") {
+        k.code = 'N';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x79") {
+        k.code = 'O';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x80") {
+        k.code = 'P';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x81") {
+        k.code = 'Q';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x82") {
+        k.code = 'R';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x83") {
+        k.code = 'S';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x84") {
+        k.code = 'T';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x85") {
+        k.code = 'U';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x86") {
+        k.code = 'V';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x87") {
+        k.code = 'W';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x88") {
+        k.code = 'X';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x89") {
+        k.code = 'Y';
+        k.mod.push_back(Mod::Shift);
+    }else if (substrings[0] == "\\x90") {
+        k.code = 'Z';
+        k.mod.push_back(Mod::Shift);
+    // LOWERCASE LETTERS
+}else if (substrings[0] == "\\x97") {
+        k.code = 'a';
+    }else if (substrings[0] == "\\x98") {
+        k.code = 'b';
+    }else if (substrings[0] == "\\x99") {
+        k.code = 'c';
+    }else if (substrings[0] == "\\x100") {
+        k.code = 'd';
+    }else if (substrings[0] == "\\x101") {
+        k.code = 'e';
+    }else if (substrings[0] == "\\x102") {
+        k.code = 'f';
+    }else if (substrings[0] == "\\x103") {
+        k.code = 'g';
+    }else if (substrings[0] == "\\x104") {
+        k.code = 'h';
+    }else if (substrings[0] == "\\x105") {
+        k.code = 'i';
+    }else if (substrings[0] == "\\x106") {
+        k.code = 'j';
+    }else if (substrings[0] == "\\x107") {
+        k.code = 'k';
+    }else if (substrings[0] == "\\x108") {
+        k.code = 'l';
+    }else if (substrings[0] == "\\x109") {
+        k.code = 'm';
+    }else if (substrings[0] == "\\x110") {
+        k.code = 'n';
+    }else if (substrings[0] == "\\x111") {
+        k.code = 'o';
+    }else if (substrings[0] == "\\x112") {
+        k.code = 'p';
+    }else if (substrings[0] == "\\x113") {
+        k.code = 'q';
+    }else if (substrings[0] == "\\x114") {
+        k.code = 'r';
+    }else if (substrings[0] == "\\x115") {
+        k.code = 's';
+    }else if (substrings[0] == "\\x116") {
+        k.code = 't';
+    }else if (substrings[0] == "\\x117") {
+        k.code = 'u';
+    }else if (substrings[0] == "\\x118") {
+        k.code = 'v';
+    }else if (substrings[0] == "\\x119") {
+        k.code = 'w';
+    }else if (substrings[0] == "\\x120") {
+        k.code = 'x';
+    }else if (substrings[0] == "\\x121") {
+        k.code = 'y';
+    }else if (substrings[0] == "\\x122") {
+        k.code = 'z';
 
 
     return k;

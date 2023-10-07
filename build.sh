955eb152852b4dbbeaedd90aaa02936c1999b863
ttibsi <ashisbitt@icloud.com>
2023-10-07 18:59:55 +0100

    fix escape segfault v1.7.0

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -xe
 
 mkdir -p ./build/
-g++ -std=c++17 ./main.cpp -o ./build/main
-g++ -std=c++17 ./escape.cpp -o ./build/escape
+g++ -std=c++20  -ggdb ./main.cpp -o ./build/main
+g++ -std=c++20 ./escape.cpp -o ./build/escape

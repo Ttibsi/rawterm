db0b8aeb2341be724d4ab2f642d09f5589c948bb
ttibsi <ashisbitt@icloud.com>
2024-08-20 18:59:43 +0100

    hex values to Color objects

--- rawterm/color.cpp
+++ rawterm/color.cpp
@@ -1,12 +1,44 @@
 #include "color.h"
 
 #include <iostream>
+#include <sstream>
+#include <stdexcept>
 #include <string>
 
 #include "core.h"
 #include "cursor.h"
 
 namespace rawterm {
+    Color::Color(const std::string& hex) {
+        if (hex.at(0) != '#') {
+            throw std::invalid_argument("Value passed not valid hex code");
+        }
+
+        if (hex.length() != 7) {
+            throw std::invalid_argument("Hex code must be 7 characters long including the #");
+        }
+
+        std::istringstream ss(hex.substr(1, 2));
+        ss >> std::hex >> red;
+
+        ss.clear();
+        ss.str(hex.substr(3, 2));
+        ss >> std::hex >> green;
+
+        ss.clear();
+        ss.str(hex.substr(5, 2));
+        ss >> std::hex >> blue;
+    }
+
+    const std::string Color::to_hex() {
+        std::ostringstream ss;
+        ss << "#" << std::uppercase << std::hex << std::setw(2) << std::setfill('0') << red
+           << std::setw(2) << std::setfill('0') << green << std::setw(2) << std::setfill('0')
+           << blue;
+
+        return ss.str();
+    }
+
     [[nodiscard]] std::string set_foreground(const std::string& s, const Color& color) {
         return "\x1B[38;2;" + std::to_string(color.red) + ';' + std::to_string(color.green) + ';' +
                std::to_string(color.blue) + 'm' + s + "\x1B[39m";
@@ -43,4 +75,5 @@ namespace rawterm {
 
         rawterm::Cursor::load_cursor_position();
     }
+
 }  // namespace rawterm
